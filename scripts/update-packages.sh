600430d13a9cc6689efcbc6a1251ca35b0c593e7
Marshall Ku <marshall@kakao.com>
2023-08-16 00:23:47 +0900

    Implement version updater script

---
+++ scripts/update-packages.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+confirm() {
+    while true; do
+        read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+        case $REPLY in
+        [yY])
+            echo
+            return 0
+            ;;
+        [nN])
+            echo
+            return 1
+            ;;
+        *) printf " \033[31m %s \n\033[0m" "invalid input" ;;
+        esac
+    done
+}
+
+folders=$(ls -d -- apps/* packages/*)
+target=$(echo "$folders" | fzf)
+current_directory="$PWD"
+
+cd "$target" || exit 1
+
+echo "$PWD"
+packages=$(pnpm outdated)
+package_name=$(sed -n 's/.*"name": "\(.*\)",/\1/p' package.json)
+IFS=$'\n'
+mapfile -t lines <<<"$packages"
+regex="│ (\S+)(.*)? │ +([0-9\.]+) +│ +([0-9\.]+) +│"
+
+cmd='pnpm i '
+
+for ((i = 0; i < ${#lines[@]}; i++)); do
+    line=${lines[$i]}
+
+    if [[ $line =~ $regex ]]; then
+        package="${BASH_REMATCH[1]}"
+        current_version="${BASH_REMATCH[3]}"
+        update_version="${BASH_REMATCH[4]}"
+
+        confirm "Will you update $package from $current_version to $update_version?" && cmd+="$package@$update_version "
+
+    fi
+done
+
+cd "$current_directory" || exit 1
+eval "$cmd --filter $package_name"
