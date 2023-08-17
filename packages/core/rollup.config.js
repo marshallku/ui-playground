9a7ded59d3ba7b8086e89d3e1a2d071bdb57adae
Marshall Ku <marshall@kakao.com>
2023-08-18 02:41:19 +0900

    Use rollup for bundling core

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,65 @@
+import { resolve } from "path";
+import rollupResolve from "@rollup/plugin-node-resolve";
+import image from "@rollup/plugin-image";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import pkg from "./package.json" assert { type: "json" };
+
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+
+const options = [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: pkg.types,
+                format: "esm",
+            },
+        ],
+        plugins: [dts()],
+        external,
+    },
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                dir: "dist",
+                format: "esm",
+                exports: "named",
+                preserveModules: true,
+                preserveModulesRoot: "src",
+            },
+        ],
+        external,
+        treeshake: {
+            moduleSideEffects: false,
+        },
+        plugins: [
+            peerDepsExternal(),
+            image(),
+            rollupResolve(),
+            commonjs({
+                include: /node_modules/,
+            }),
+            typescript({ useTsconfigDeclarationDir: true }),
+            postcss({
+                extract: false,
+                modules: true,
+                use: [
+                    [
+                        "sass",
+                        {
+                            data: "",
+                            includePaths: [resolve("./styles")],
+                        },
+                    ],
+                ],
+            }),
+        ],
+    },
+];
+
+export default options;
