925b3965702a4a9341930fd51dcb499311c99dd2
Marshall Ku <marshall@kakao.com>
2023-08-24 23:45:51 +0900

    Implement util for parsing class names from sass module

---
+++ packages/utils/src/lib/classNames.ts
@@ -0,0 +1,51 @@
+export default function classNames(styles: Record<string, string> = {}, rootClassName = "") {
+    const hasOwn = {}.hasOwnProperty;
+    const func = (...classNames: unknown[]) => {
+        const classes: unknown[] = [];
+
+        if (classNames.length === 0) {
+            // It's root
+            classes.push(styles[rootClassName] || rootClassName);
+        }
+
+        for (let i = 0, max = classNames.length; i < max; i++) {
+            const name = classNames[i];
+
+            // HACK: To add extra modifier in root
+            if (name === "") {
+                classes.push(styles[rootClassName] || rootClassName);
+            }
+
+            if (!name) {
+                continue;
+            }
+
+            const nameType = typeof name;
+
+            if (nameType === "string" || nameType === "number") {
+                const nameWithRoot = rootClassName + name;
+                classes.push(styles[nameWithRoot] || nameWithRoot);
+                continue;
+            }
+
+            if (Array.isArray(name)) {
+                func(name);
+                continue;
+            }
+
+            if (nameType === "object") {
+                // It's not null and array, so it must be an object.
+                for (const key in name as object) {
+                    const keyWithRoot = rootClassName + key;
+                    if (hasOwn.call(name, keyWithRoot) && name[keyWithRoot as keyof typeof name]) {
+                        classes.push(styles[keyWithRoot] || keyWithRoot);
+                    }
+                }
+            }
+        }
+
+        return classes.join(" ");
+    };
+
+    return func;
+}
