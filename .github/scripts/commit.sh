1ed2c0e9ac114db87a1de802dfe1f6e6b94bdf41
Marshall Ku <marshall@kakao.com>
2023-08-30 22:01:18 +0900

    Add script for committing files

---
+++ .github/scripts/commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ -z $(git status --porcelain) ]]; then
+    exit 0
+fi
+
+files_to_commit=${FILES:-"-A"}
+
+git config user.name github-actions[bot]
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+git add "$files_to_commit"
+git commit -m "$MESSAGE"
+git push
