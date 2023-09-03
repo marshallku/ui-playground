b67b0b5d8694bf189fd6014474fde5431469ebd6
Marshall Ku <marshall@kakao.com>
2023-09-03 23:36:04 +0900

    Move utils to seperated files

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-confirm() {
-    while true; do
-        read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
-        case $REPLY in
-        [yY])
-            echo
-            return 0
-            ;;
-        [nN])
-            echo
-            return 1
-            ;;
-        *) printf " \033[31m %s \n\033[0m" "invalid input" ;;
-        esac
-    done
-}
+. scripts/utils/confirm.sh
 
 folders=$(ls -d -- apps/* packages/*)
 target=$(echo "$folders" | fzf)
---
+++ scripts/utils/confirm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+confirm() {
+    while true; do
+        read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+        case $REPLY in
+        [yY])
+            echo
+            return 0
+            ;;
+        [nN])
+            echo
+            return 1
+            ;;
+        *) printf " \033[31m %s \n\033[0m" "invalid input" ;;
+        esac
+    done
+}
