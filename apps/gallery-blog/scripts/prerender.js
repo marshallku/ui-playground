fd909f271600122b97fd6313c97c4ad0bca33530
Marshall Ku <marshall@kakao.com>
2023-09-29 23:38:14 +0900

    Implement metadata parser

--- apps/gallery-blog/scripts/prerender.js
+++ apps/gallery-blog/scripts/prerender.js
@@ -3,10 +3,34 @@ import { resolve } from "path";
 
 const POST_PATH = "posts";
 
+/**
+ * Get meta data from given markdown string
+ *
+ * @param {string} fileName
+ * @param {string} content
+ */
+const getMetaDataFromMarkdown = (fileName, content) => {
+    const [, metaDataContent] = content.split("---");
+
+    if (!metaDataContent) {
+        throw new Error(`Invalid file: ${fileName}`);
+    }
+
+    return Object.fromEntries(
+        metaDataContent
+            .split("\n")
+            .filter(Boolean)
+            .map((line) => {
+                const [key, ...value] = line.split(":");
+                return [key.trim(), value.join(":").trim()];
+            }),
+    );
+};
+
 const generateFile = (fileName) => {
     const content = readFileSync(resolve(fileName), "utf8");
 
-    console.log(content);
+    console.log(fileName, getMetaDataFromMarkdown(fileName, content));
 };
 
 const files = readdirSync(resolve(POST_PATH));
