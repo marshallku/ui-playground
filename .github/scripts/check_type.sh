a896333c0ccbae10074a5718ff676b8c755b2cb1
Marshall Ku <marshall@kakao.com>
2023-10-11 00:19:52 +0900

    Add ci pipeline for checking types of modified packages and apps

---
+++ .github/scripts/check_type.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+declare -A arr
+
+while IFS= read -r line; do
+    if [[ $line == apps/* ]] || [[ $line == packages/* ]]; then
+        IFS='/' read -ra ADDR <<<"$line"
+        path="${ADDR[0]}/${ADDR[1]}"
+        arr[$path]=1
+    fi
+done < <(git diff --name-only --diff-filter=duxb "$BASE" "$HEAD" | grep -E '\.((j|t)sx?)$')
+
+for key in "${!arr[@]}"; do
+    package_file="$key/package.json"
+
+    if [[ ! -f "$package_file" ]]; then
+        continue
+    fi
+
+    if ! grep -q '"build": ' "$package_file"; then
+        continue
+    fi
+
+    package_name=$(grep -oP '"name"\s*:\s*"\K[^"]*' "$package_file")
+
+    pnpm run build --filter="$package_name" || exit 1
+done
