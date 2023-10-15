fa8f1592847646450077b3b729668ef088841ba4
Marshall Ku <marshall@kakao.com>
2023-10-15 10:50:28 +0900

    Update eslint configuration

--- packages/eslint-config-marshallku/index.js
+++ packages/eslint-config-marshallku/index.js
@@ -1,9 +1,41 @@
 module.exports = {
-    extends: ["next", "turbo", "prettier"],
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "next",
+        "turbo",
+        "eslint-config-prettier",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+    ],
+    plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
+    settings: { react: { version: "detect" } },
     rules: {
+        "prettier/prettier": "error",
+        "no-implicit-coercion": "error",
+        "no-console": "error",
+
+        "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+        "@typescript-eslint/naming-convention": [
+            "error",
+            { format: ["camelCase", "PascalCase"], selector: "function" },
+            { format: ["PascalCase"], selector: ["interface", "typeAlias"] },
+            {
+                format: ["camelCase", "PascalCase", "UPPER_CASE"],
+                selector: "variable",
+                leadingUnderscore: "allow",
+            },
+        ],
+
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "react/display-name": "off",
+
         "@next/next/no-html-link-for-pages": "off",
     },
     parserOptions: {
+        ecmaFeatures: { jsx: true },
+        jsx: true,
+        useJSXTextNode: true,
         babelOptions: {
             presets: [require.resolve("next/babel")],
         },
