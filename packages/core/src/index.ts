b5efb7eb53cd0d75d8373100e6268e1d0fa6a561
Marshall Ku <marshall@kakao.com>
2023-10-15 10:54:39 +0900

    Fix eslint problems

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
 export { Button, type ButtonProps } from "./Button";
-export * from './Icon';
-export { default as Icon } from './Icon';
+export * from "./Icon";
+export { default as Icon } from "./Icon";
