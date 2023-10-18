bd637a49fe71d31a3303b2fd8ea95ed69897daaa
Marshall Ku <marshall@kakao.com>
2023-10-19 00:08:28 +0900

    Add theme in next.js app

---
+++ apps/next/app/provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ThemeProvider } from "@marshallku/core";
+import { ReactNode, useEffect, useState } from "react";
+
+interface ProviderProps {
+    children?: ReactNode;
+}
+
+export default function Provider({ children }: ProviderProps) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    // for persistent theme page.
+    if (!mounted) {
+        return <></>;
+    }
+
+    return <ThemeProvider>{children}</ThemeProvider>;
+}
