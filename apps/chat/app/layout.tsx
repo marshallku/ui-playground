0d7f263f24415128298b644fdb63dbb0feee51e5
Marshall Ku <marshall@kakao.com>
2023-11-04 23:23:21 +0900

    Modify global designs of chat app

--- apps/chat/app/layout.tsx
+++ apps/chat/app/layout.tsx
@@ -3,9 +3,7 @@ import "@marshallku/core/dist/palette.css";
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -15,7 +13,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    as="style"
+                    crossOrigin=""
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+                />
+            </Head>
+            <body>{children}</body>
         </html>
     );
 }
