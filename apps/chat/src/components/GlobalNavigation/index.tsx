6140f87d3724654db3e458789c66d6f1ab2cb9ca
Marshall Ku <marshall@kakao.com>
2023-11-06 00:03:15 +0900

    Add props for modifying GlobalNavigation

--- apps/chat/src/components/GlobalNavigation/index.tsx
+++ apps/chat/src/components/GlobalNavigation/index.tsx
@@ -1,18 +1,45 @@
-import Logo from "#components/Logo";
-import { classNames } from "@marshallku/utils";
-import styles from "./index.module.scss";
-import Link from "next/link";
+"use client";
 
-export interface GlobalNavigationProps {}
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Button, Typography } from "@marshallku/core";
+import { classNames } from "@marshallku/utils";
+import { Logo } from "#components";
+import styles from "./index.module.scss";
+
+export interface GlobalNavigationProps {
+    /** title of current page */
+    title?: string;
+    /** determines style of navigation */
+    newTab?: boolean;
+}
 
 const cx = classNames(styles, "global-navigation");
 
-function GlobalNavigation({}: GlobalNavigationProps) {
+function GlobalNavigation({ title, newTab }: GlobalNavigationProps) {
+    const router = useRouter();
+
     return (
         <nav className={cx()}>
-            <Link href="/" className={cx("__home")}>
-                <Logo className={cx("__logo")} />
-            </Link>
+            {newTab ? (
+                <Button
+                    onClick={() => {
+                        router.back();
+                    }}
+                    variant="text"
+                >
+                    {/* FIXME: Use icon instead */}←
+                </Button>
+            ) : (
+                <Link href="/" className={cx("__home")}>
+                    <Logo className={cx("__logo")} />
+                </Link>
+            )}
+            {title && (
+                <Typography variant="b2" fontWeight={500}>
+                    {title}
+                </Typography>
+            )}
         </nav>
     );
 }
