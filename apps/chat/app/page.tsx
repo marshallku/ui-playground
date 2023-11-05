55bbe4ac3d2575f24553bc5a5bf9d543f8e1f1fb
Marshall Ku <marshall@kakao.com>
2023-11-05 16:36:52 +0900

    Update design of main page

--- apps/chat/app/page.tsx
+++ apps/chat/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { Typography } from "@marshallku/core";
+import { classNames } from "@marshallku/utils";
 import { Button } from "#components";
 import styles from "./page.module.scss";
 
+const cx = classNames(styles, "home");
+
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <Typography component="h1">Chat app</Typography>
-            <div>
+        <main className={cx()}>
+            <Typography variant="h1" component="h1" fontWeight={700} marginBottom={24} className={cx("__title")}>
+                Chat app
+            </Typography>
+            <div className={cx("__buttons")}>
                 <Button href="/login" size="large" horizontalResizing="fill" variant="primary">
                     Login
                 </Button>
