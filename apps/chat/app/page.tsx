46dbf85293b0293337709bca0122d5f72ac80a7f
Marshall Ku <marshall@kakao.com>
2023-11-05 15:37:35 +0900

    Update main page

--- apps/chat/app/page.tsx
+++ apps/chat/app/page.tsx
@@ -1,5 +1,21 @@
-import styles from "./page.module.css";
+"use client";
+
+import Link from "next/link";
+import { Button, Typography } from "@marshallku/core";
+import styles from "./page.module.scss";
 
 export default function Home() {
-    return <main className={styles.main}>Hello world</main>;
+    return (
+        <main className={styles.main}>
+            <Typography component="h1">Chat app</Typography>
+            <div>
+                <Button component={Link} href="/login" size="large" horizontalResizing="fill" variant="primary">
+                    Login
+                </Button>
+                <Button component={Link} href="/sign-up" size="large" horizontalResizing="fill">
+                    Sign up
+                </Button>
+            </div>
+        </main>
+    );
 }
