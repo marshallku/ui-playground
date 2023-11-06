4945274dcf0033da71361402a9d071824843b815
Marshall Ku <marshall@kakao.com>
2023-11-06 22:21:35 +0900

    Add form validating and login logics

--- apps/chat/src/templates/Login/index.tsx
+++ apps/chat/src/templates/Login/index.tsx
@@ -1,23 +1,47 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input, Typography } from "@marshallku/core";
 import { classNames } from "@marshallku/utils";
-import { login } from "app/actions";
+import { authLogin } from "#api";
 import { Button } from "#components";
 import styles from "./index.module.scss";
 
 const cx = classNames(styles, "login");
+const loginSchema = z.object({
+    name: z.string().min(1, "ID is required"),
+    password: z.string().min(1, "Password is required"),
+});
 
 function LoginTemplate() {
+    const { register, handleSubmit } = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+    });
+    const router = useRouter();
+
     return (
         <div className={cx()}>
             <Typography variant="h2" fontWeight={700} marginBottom={2}>
                 Welcome back!
             </Typography>
             <Typography marginBottom={24}>You should be logged in for using this service.</Typography>
-            <form className={cx("__form")} action={login}>
-                <Input name="username" label="ID" />
-                <Input type="password" name="password" label="Password" />
+            <form
+                className={cx("__form")}
+                onSubmit={handleSubmit(async (formData) => {
+                    const data = await authLogin(formData);
+
+                    if ("statusCode" in data) {
+                        return;
+                    }
+
+                    router.push("/");
+                })}
+            >
+                <Input label="ID" {...register("name")} />
+                <Input type="password" label="Password" {...register("password")} />
                 <div className={cx("__buttons")}>
                     <Button type="submit" variant="primary" size="large" horizontalResizing="fill">
                         Login
