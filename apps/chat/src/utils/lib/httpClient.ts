d2e35fc7e44dc2a875c8864b7414f04b80befd8c
Marshall Ku <marshall@kakao.com>
2023-11-15 07:56:17 +0900

    Implement utility for creating http client

---
+++ apps/chat/src/utils/lib/httpClient.ts
@@ -0,0 +1,48 @@
+type FetchParameters = Parameters<typeof fetch>;
+type Promiseable<T> = T | Promise<T>;
+
+export interface HTTPClientOption<T = Response> extends Omit<NonNullable<FetchParameters[1]>, "body"> {
+    baseUrl?: string;
+    interceptors?: {
+        request?(init: NonNullable<FetchParameters[1]>): Promiseable<FetchParameters[1]>;
+        response?(response: Response): Promiseable<T>;
+    };
+}
+
+const applyBaseUrl = (input: FetchParameters[0], baseUrl?: string) => {
+    if (!baseUrl) {
+        return input;
+    }
+
+    if (typeof input === "object") {
+        if ("url" in input) {
+            return new URL(input.url, baseUrl);
+        }
+
+        return new URL(input, baseUrl);
+    }
+
+    const url = new URL(input, baseUrl);
+    return url.toString();
+};
+
+export default function httpClient<T = Response>({
+    baseUrl,
+    interceptors = {},
+    ...requestInit
+}: HTTPClientOption<T> = {}) {
+    return async function <R = T extends Response ? Response : T>(
+        input: FetchParameters[0],
+        init?: FetchParameters[1],
+    ): Promise<R> {
+        const option = { ...requestInit, ...init };
+        const interceptorAppliedOption = interceptors.request ? await interceptors.request(option) : option;
+        const response = await fetch(applyBaseUrl(input, baseUrl), interceptorAppliedOption);
+
+        if (interceptors.response) {
+            return (await interceptors.response(response)) as R;
+        }
+
+        return response as R;
+    };
+}
