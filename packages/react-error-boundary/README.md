374aaa12d5a8653d4bdae8932b0f4db0e35d8cbc
Marshall Ku <marshall@kakao.com>
2023-11-28 07:55:53 +0900

    Add react-error-boundary

---
+++ packages/react-error-boundary/README.md
@@ -0,0 +1,6 @@
+# react-error-boundary
+
+Original source from [react-error-boundary](https://github.com/bvaughn/react-error-boundary).\
+Last commit hash: [23a4d77](https://github.com/bvaughn/react-error-boundary/commit/23a4d779744f3079dfe0960acaa4bdfd5dede87b)
+
+I've forked this to test integration with a [self-hosted sentry](https://github.com/getsentry/self-hosted)
---
+++ packages/react-error-boundary/src/lib/withErrorBoundary.ts
@@ -0,0 +1,26 @@
+import {
+    createElement,
+    forwardRef,
+    ForwardedRef,
+    RefAttributes,
+    ForwardRefExoticComponent,
+    PropsWithoutRef,
+    ComponentType,
+} from "react";
+import { ErrorBoundary } from "./ErrorBoundary";
+import { ErrorBoundaryProps } from "./types";
+
+export function withErrorBoundary<Props extends object>(
+    component: ComponentType<Props>,
+    errorBoundaryProps: ErrorBoundaryProps,
+): ForwardRefExoticComponent<PropsWithoutRef<Props> & RefAttributes<any>> {
+    const Wrapped = forwardRef<ComponentType<Props>, Props>((props: Props, ref: ForwardedRef<ComponentType<Props>>) =>
+        createElement(ErrorBoundary, errorBoundaryProps, createElement(component, { ...props, ref })),
+    );
+
+    // Format for display in DevTools
+    const name = component.displayName || component.name || "Unknown";
+    Wrapped.displayName = `withErrorBoundary(${name})`;
+
+    return Wrapped;
+}
