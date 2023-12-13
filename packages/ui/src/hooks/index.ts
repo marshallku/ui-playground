a74d8fc943e372a81e56b6cd31d86c7c20934f45
Marshall Ku <marshall@kakao.com>
2023-12-13 23:30:05 +0900

    Add hook for tracing state updates

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useIntersectionObserver } from "./lib/useIntersectionObserver";
 export { default as useLocalStorage } from "./lib/useLocalStorage";
+export { default as useTraceUpdate } from "./lib/useTraceUpdate";
---
+++ packages/ui/src/hooks/lib/useTraceUpdate.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef } from "react";
+
+export default function useTraceUpdate<T extends Record<string, any> = Record<string, any>>(props: T) {
+    const prev = useRef(props);
+    useEffect(() => {
+        const changedProps = Object.entries(props).reduce(
+            (ps, [k, v]) => {
+                if (prev.current[k] !== v) {
+                    ps[k as keyof typeof ps] = [prev.current[k], v];
+                }
+                return ps;
+            },
+            {} as Partial<Record<keyof T, [any, any]>>,
+        );
+        if (Object.keys(changedProps).length > 0) {
+            // eslint-disable-next-line no-console
+            console.log("Changed props:", changedProps);
+        }
+        prev.current = props;
+    });
+}
